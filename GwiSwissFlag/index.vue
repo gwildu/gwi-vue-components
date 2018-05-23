117434d6bdd5c8ca940d32d9fbd898949b12002d
Marius Rickenbacher <marius.rickenbacher@ricardo.ch>
2018-05-23 07:53:18 +0200

    add thai and swiss flag components

---
+++ GwiSwissFlag/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container" :style="{height: `${height}px`, width: `${height}px`}">
+    <div class="row red">&nbsp;</div>
+    <div class="row">
+      <div class="red">&nbsp;</div>
+      <div class="red">&nbsp;</div>
+      <div class="white">&nbsp;</div>
+      <div class="red">&nbsp;</div>
+      <div class="red">&nbsp;</div>
+    </div>
+    <div class="row">
+      <div class="red">&nbsp;</div>
+      <div class="white">&nbsp;</div>
+      <div class="white">&nbsp;</div>
+      <div class="white">&nbsp;</div>
+      <div class="red">&nbsp;</div>
+    </div>
+    <div class="row">
+      <div class="red">&nbsp;</div>
+      <div class="red">&nbsp;</div>
+      <div class="white">&nbsp;</div>
+      <div class="red">&nbsp;</div>
+      <div class="red">&nbsp;</div>
+    </div>
+    <div class="red row">&nbsp;</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'GwiSwissFlag',
+    props: {
+      height: {
+        default: 500,
+        type: Number,
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+  }
+  .row {
+    display: flex;
+    height: 20%;
+    align-content: stretch;
+  }
+  .red {
+    background-color: red;
+    flex-grow: 1;
+  }
+  .white {
+    background-color: white;
+    flex-grow: 1;
+  }
+</style>
---
+++ GwiThaiFlag/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="container" :style="{height: `${height}px`, width: `${height * 1.5}px`}">
+    <div class="row red">&nbsp;</div>
+    <div class="row white">&nbsp;</div>
+    <div class="row blue">&nbsp;</div>
+    <div class="row blue">&nbsp;</div>
+    <div class="row white">&nbsp;</div>
+    <div class="row red">&nbsp;</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "GwiThaiFlag",
+    props: {
+      height: {
+        default: 500,
+        type: Number,
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .container {
+    display: flex;
+    flex-direction: column;
+    position: relative;
+  }
+  .row {
+    display: flex;
+    height: 16.666666666666667%;
+    align-content: stretch;
+  }
+  .red {
+    background-color: red;
+    flex-grow: 1;
+  }
+  .white {
+    background-color: white;
+    flex-grow: 1;
+  }
+  .blue {
+    background-color: blue;
+    flex-grow: 1;
+  }
+</style>
